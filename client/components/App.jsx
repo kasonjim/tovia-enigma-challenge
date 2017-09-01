52de1c1dda6a0eac9d27a48213cfd84fa8461b42
Jason Kim <kasonjim@gmail.com>
2017-09-01 13:13:04 -0700

    Initialize react structure

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+window.App = App;

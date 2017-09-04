c62f851cb7ae6b71922cc5d99f5ce4e9aa272aa3
Jason Kim <kasonjim@gmail.com>
2017-09-04 16:17:01 -0700

    Update form styling with Card component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,8 @@
+import { Card, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
+import Avatar from 'react-toolbox/lib/avatar';
+import Input from 'react-toolbox/lib/input';
+import DatePicker from 'react-toolbox/lib/date_picker';
 import { Button } from 'react-toolbox/lib/button';
-import { Input } from 'react-toolbox/lib/input';
-import { DatePicker } from 'react-toolbox/lib/date_picker';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -31,13 +33,45 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Tovias Enigma</h1>
-        <Input type="text" label="Name" name="name" required onChange={this.inputHandler.bind(this, 'name')} />
-        <Input type="text" label="Message" name="message" maxLength={120} multiline required onChange={this.inputHandler.bind(this, 'message')} />
-        <DatePicker label="Expiration Date" name="date" required onChange={this.inputHandler.bind(this, 'date')} />
-        <Button label="ENCRYPT" onMouseUp={this.encrypt.bind(this)} />
-        <Button label="DECRYPT" onMouseUp={this.decrypt.bind(this)} />
+      <div className="test">
+        <Card style={{width: '450px'}}>
+          <CardTitle
+            title="Tovia's Enigma" />
+          <CardText>
+            <Avatar title={this.state.name || '?'} />
+            <Input
+              type="text"
+              label="Name"
+              name="name"
+              required
+              onChange={this.inputHandler.bind(this, 'name')} />
+            <Input
+              type="text"
+              label="Message"
+              name="message"
+              maxLength={120}
+              multiline
+              required
+              onChange={this.inputHandler.bind(this, 'message')} />
+            <DatePicker
+              label="Expiration Date"
+              name="date"
+              required
+              onChange={this.inputHandler.bind(this, 'date')} />
+          </CardText>
+          <CardActions>
+            <Button
+              label="ENCRYPT"
+              onMouseUp={this.encrypt.bind(this)} />
+            <Button
+              label="DECRYPT"
+              onMouseUp={this.decrypt.bind(this)} />
+          </CardActions>
+          <CardText>
+            <div>Your Passphrase - <a href="">Some Random String</a></div>
+            <div><a href="">Generate new Passphrase</a></div>
+          </CardText>
+        </Card>
       </div>
     );
   }
